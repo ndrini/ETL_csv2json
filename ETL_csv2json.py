38c823f311df830438211c72d28c82f7e44e49a2
ndrini <ndrini@gmail.com>
2018-06-08 06:15:57 +0200

    structured json test

--- ETL_csv2json.py
+++ ETL_csv2json.py
@@ -3,19 +3,21 @@
 
 # NIF is mostly a number; so, just by divide if by 23 we get the last digit: the verification one
 
-# class ETL: 
+class ETL(): 
 
-def encode2json_text():
-    return "{"
+    def encode2json_text(self, data_list):
+        return "{product_Banana}"
+        
 
-def separate(NIF):
-    SPLIT_DIGIT = 8 
-    first_part = NIF[:SPLIT_DIGIT]
-    last_part   = NIF[SPLIT_DIGIT:]
-    return first_part,last_part
+    def separate(self):
+        SPLIT_DIGIT = 8 
+        first_part = NIF[:SPLIT_DIGIT]
+        last_part   = NIF[SPLIT_DIGIT:]
+        return first_part,last_part
         
  
 if __name__ == '__main__':
-    pass    
+    etl=ETL()
+    print etl.encode2json_text()[0]
     #assert last_digit("S9126639E") == False, "vero"
-    print encode2json_text()[0]
+    
--- ETL_csv2json_test.py
+++ ETL_csv2json_test.py
@@ -8,6 +8,8 @@ import unittest
 import ETL_csv2json as C2J  
 # import led_lamps as ll
 
+ETL=C2J.ETL()
+
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
         pass
@@ -15,11 +17,12 @@ class TestStringMethods(unittest.TestCase):
     def test_encode2json_text(self):
         data_list = [   { "column_A":"product_Ana", "column_B":1, "column_C":34.39 },
                         { "column_A":"product_Banana", "column_B":3, "column_C":14.39 },]
+        self.assertEqual(ETL.encode2json_text(data_list)[0], "{")
 
-        self.assertEqual(True, True)
-
-        self.assertEqual(C2J.encode2json_text()[0], "{")
-
+        my_json = ETL.encode2json_text(data_list)
+        self.assertEqual(my_json[0], "{")
+        self.assertEqual(my_json[-1], "}")
+        self.assertIn("product_Banana", my_json)
  
 if __name__ == '__main__':
     unittest.main()
